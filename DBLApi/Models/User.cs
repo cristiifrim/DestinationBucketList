29f5f8ae58071203739d737ea3bb83c58e11dbc2
cristiifrim <i.cristian02@yahoo.com>
2023-05-18 21:32:47 +0300

    added user model + db context

---
+++ DBLApi/Models/User.cs
@@ -0,0 +1,11 @@
+namespace DBLApi.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Username { get; set; } = default!;
+        public string Password { get; set; } = default!;
+        public string Email { get; set; } = default!;
+        public string Role { get; set; } = Roles.User;
+    }
+}
---
+++ DBLApi/Repositories/Interfaces/IGenericRepository.cs
@@ -0,0 +1,16 @@
+namespace DBLApi.Repositories.Interfaces
+{
+    public interface IGenericRepository<T> where T : class
+    {
+        Task<T?> GetById(int id);
+        Task<IEnumerable<T>> GetAll(int page = 1, int pageSize=30);
+
+        Task<int> NumberOfPages(int pageSize = 30);
+
+        Task<bool> Add(T entity);
+        Task<bool> Update(T entity);
+        Task<bool> Delete(T entity);
+
+        Task<bool> SaveChanges();
+    }
+}
