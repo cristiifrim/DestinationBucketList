0a8c95e5639684d5bd53a2af20ddbff2312b182d
913-Hornea-Dorian-Alexandru <alex.1375@yahoo.com>
2023-05-18 22:28:17 +0300

    Update Destination.cs

--- DBLApi/Models/Destination.cs
+++ DBLApi/Models/Destination.cs
@@ -3,11 +3,11 @@ namespace DBLApi.Models
     public class Destination
     {
         protected int Id { get; set; }
-        protected string? Geolocation { get; set; }
-        protected string? Title { get; set; }
-        protected string? Image { get; set; }
-        protected string? Description { get; set; }
-        protected DateTime? StartDate { get; set; }
-        protected DateTime? EndDate { get; set; }
+        protected string Geolocation { get; set; } = default!;
+        protected string Title { get; set; } = default!;
+        protected string Image { get; set; } = default!;
+        protected string Description { get; set; } = default!;
+        protected DateOnly StartDate { get; set; }
+        protected DateOnly EndDate { get; set; } 
     }
 }
